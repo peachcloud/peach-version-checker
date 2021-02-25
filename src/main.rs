c07993295d55095d7de508cd069390bbb80ff10b
mycognosist <gnomad@cryptolab.net>
2021-02-25 09:19:54 +0000

    Add devdocs version checker

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
 use anyhow::Result;
 use regex::Regex;
 
-const DOCS_URL: &'static str = "http://docs.peachcloud.org/software";
-const GITHUB_URL: &'static str = "https://github.com/peachcloud";
+const DOCS_URL: &str = "http://docs.peachcloud.org/software";
+const GITHUB_URL: &str = "https://github.com/peachcloud";
 
+#[derive(Debug)]
 struct Service {
     name: String,
+    docs_url: String,
     repo_url: String,
-    //repo_version: str,
-    //docs_url: str,
-    //docs_version: str,
+    readme_version: Option<String>,
+    docs_version: Option<String>,
     //manifest_version: Option<str>,
     //verified: bool,
 }
 
 impl Service {
     fn new(name: String) -> Self {
+        let docs_url = format!("{}/{}/{}{}", DOCS_URL, "microservices", name, ".html");
         let repo_url = format!("{}/{}", GITHUB_URL, name);
 
-        Service { name, repo_url }
+        Service {
+            name,
+            docs_url,
+            repo_url,
+            docs_version: None,
+            readme_version: None,
+        }
     }
 
-    fn readme_version(self, body: String) -> Option<String> {
-        regex_finder(r"badge/version-(.*)-%3C", &body)
+    fn readme_version(&mut self, body: String) {
+        if let Some(version) = regex_finder(r"badge/version-(.*)-%3C", &body) {
+            self.readme_version = Some(version)
+        }
+    }
+
+    fn docs_version(&mut self, body: String) {
+        if let Some(version) = regex_finder(r"badge/version-(.*)-%3C", &body) {
+            self.docs_version = Some(version)
+        }
     }
 }
 
@@ -36,14 +52,15 @@ fn regex_finder(pattern: &str, text: &str) -> Option<String> {
 }
 
 fn main() -> Result<()> {
-    let buttons = Service::new("peach-buttons".to_string());
+    let mut oled = Service::new("peach-oled".to_string());
 
-    let body = reqwest::blocking::get(&buttons.repo_url)?.text()?;
+    let repo_body = reqwest::blocking::get(&oled.repo_url)?.text()?;
+    let docs_body = reqwest::blocking::get(&oled.docs_url)?.text()?;
 
-    match buttons.readme_version(body) {
-        Some(version) => println!("{}", version),
-        None => ()
-    }
+    oled.readme_version(repo_body);
+    oled.docs_version(docs_body);
+
+    println!("{:?}", oled);
 
     Ok(())
 }
